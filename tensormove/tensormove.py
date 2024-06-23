ef48ca37aedfe48653e19e3b0b3d3f2637d7b1a2
Peter <phorrigan@gmail.com>
2024-06-22 20:50:11 -0700

    Update tensormove.py

--- tensormove/tensormove.py
+++ tensormove/tensormove.py
@@ -1,17 +1,36 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class PyTorchModel(nn.Module):
     def __init__(self):
         super(PyTorchModel, self).__init__()
-        self.fc1 = nn.Linear(784, 256)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(256, 10)
-
+        self.fc1 = nn.Linear(784, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 10)
+        self.dropout = nn.Dropout(0.5)
+        
+        self.init_weights()
+        
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+                nn.init.constant_(m.bias, 0)
+    
     def forward(self, x):
         x = self.fc1(x)
-        x = self.relu(x)
+        x = F.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc3(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc4(x)
         return x
 
+# Instantiate the model
 pytorch_model = PyTorchModel()
